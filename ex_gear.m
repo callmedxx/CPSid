234e477f762cd698e98d0b47072eb9816fd001d4
Huazhong Artificial Intelligence Lab (HAIL) <yye@hust.edu.cn>
2018-05-18 09:21:44 +0800

    Update ex_gear.m

--- ex_gear.m
+++ ex_gear.m
@@ -1,4 +1,3 @@
-
 clear
 clc
 close all
@@ -11,32 +10,53 @@ addpath('./savemat');
 %% design
 
 
-load('current.mat')
-
+load('circle.mat')
+load('nomarlex8.mat')
 %% make dic
+state  = zeros(size(circle));
+state(1:19995) = 1;
+t = 0:0.001:length(circle)*0.001;
 
-index = 1:300:length(current);
-current = current(index);
+index = 1:300:length(circle);
+current = circle(index);
 state = state(index);
+t1= t(index)';
+
+
+
 
 y = current(2:end);
 state = state(2:end);
+t1= t1(2:end);
 x = current(1:end-1);
 polyorder = 2;
 usesine = 0;
 memory =5;
 basis_function.work = 'off';
 dic= library(x,polyorder,usesine,memory,basis_function);
-
-A = dic(memory+2:end,:);
-y = y(memory+2:end,:);
-
-state = state(memory+2:end,:);
+% dic(:,2) = [];
+A = dic(memory+1:end,:);
+y = y(memory+1:end,:);
+t1 = t1(memory+1:end,:);
+state = state(memory+1:end,:);
+A111 = A;
+y111 = y;
+index = 1:64;
+A = A(index,:);
+y = y (index,:);
+state = state(index,:);
+t1 = t1(index,:);
 
 %% identify subsystem
 
+% 
+% parameter.lambda = [1.5 0.00001];   % 65
+% parameter.MAXITER = 5;
+% parameter.max_s = 20;%the max s
+% parameter.epsilon = [1e-4  0.026];
 
-parameter.lambda = [0.3 0.00001];   % the lambda of z in algorithm 1.
+
+parameter.lambda = [1.5 0.00005];   % 64
 parameter.MAXITER = 5;
 parameter.max_s = 20;%the max s
 parameter.epsilon = [1e-4  0.026];
@@ -63,6 +83,9 @@ idx_sys = final_result.idx;
 
 
 %%
+
+
+% % clear syslogic labelMat data para_log Phi2
 % Phi2 = [ones(size(x)) x ];
 t = 1:length(y);
 
@@ -92,29 +115,21 @@ A1 = A(index1,:);
 ansy1 = 1000000*ones(size(y11(:,1)));
 ansy1(find(judge11==0)) = y11(find(judge11==0));
 ansy1(ansy1==1000000)=nan;
-
+plot(ansy1,'b','Linewidth',5);
 
 ansy11 = 1000000*ones(size(y11(:,1)));
-ansy11(idx_sys{1},:) = A1(idx_sys{1},:)*sys(:,1);
+ansy11(idx_sys{2},:) = A1(idx_sys{2},:)*sys(:,2);
 ansy11(ansy11==1000000)=nan;
-
+plot(ansy11,'bo','MarkerSize',10)
 err1 = ansy1 - ansy11;
 
 ansy2 = 1000000*ones(size(y11(:,1)));
 ansy2(find(judge11==1)) = y11(find(judge11==1));
 ansy2(ansy2==1000000)=nan;
-
+plot(ansy2,'r','Linewidth',5)
 
 ansy22 = 1000000*ones(size(y11(:,1)));
-ansy22(idx_sys{2},:) = A1(idx_sys{2},:)*sys(:,2);
+ansy22(idx_sys{1},:) = A1(idx_sys{1},:)*sys(:,1);
 ansy22(ansy22==1000000)=nan;
-
-err2 = ansy2 - ansy22;
-
-plot(ansy2,'r','Linewidth',5)
 plot(ansy22,'ro','MarkerSize',10)
-plot(ansy1,'b','Linewidth',5);
-plot(ansy11,'bo','MarkerSize',10)
-legend('normal','subsystem1','fault','subsystem2')
-
-
+err2 = ansy2 - ansy22;
